f6706f2713cec7ae923a2df0affe489572e1ff33
raghuprasadks <prasadraghuks@gmail.com>
2024-02-19 11:38:37 +0530

    initial version

---
+++ programs/python/2-DataTypes.py
@@ -0,0 +1,20 @@
+course="AI ML"
+print("I am learning ",course)
+print("data type of course is ",type(course))
+"""
+Data types - str,int,float,bool
+list,tuple,dictionary,set
+"""
+courselist = ["C","Java","Python"]
+print("course list ",courselist)
+courselist.append("C#")
+courselist.append("Java Script")
+print("after append ",courselist)
+print("element at 0 th index ",courselist[0])
+for course in courselist:
+    print(course)
+print("update")
+courselist[0]="C Program"
+courselist.pop()
+
+
